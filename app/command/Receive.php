47579bbe8c2dbf5db1b18f5c077bdcbdb61862ab
StilesZ <1350054642@qq.com>
2022-03-11 16:54:51 +0800

    amqp 消息队列封装

--- app/command/Receive.php
+++ app/command/Receive.php
@@ -4,6 +4,7 @@
 namespace app\command;
 
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 use rabbit\Client;
 use think\console\Command;
 use think\console\Input;
@@ -13,9 +14,16 @@ class Receive extends Command
 {
     protected function configure()
     {
-        $this->setName('TaskClose')->setDescription('Here is the TaskClose everyday');
+        // 指令配置
+        $this->setName('receive')
+            ->setDescription('rabbitmq 消费队列');
     }
     protected function execute(Input $input, Output $output) {
-        Client::instance('test')->rMq(1);
+        // 指令输出
+        $output->writeln("RabbitMQ 消费队列开始启动……\n");
+        $mqData = Client::instance('test')->rMq(1);
+
+        $output->writeln("RabbitMQ 消费队列完成……\n");
+        dump($mqData) ;
     }
 }
--- app/extra/rabbitmq.php
+++ app/extra/rabbitmq.php
@@ -18,6 +18,26 @@ return [
                 'consumer' => 'DealTest' // 消费地址
             ]
         ]
+    ],
+
+    'localhost' => [                                //连接名称
+        "conn" => [
+            'host' => '127.0.0.1',             //连接地址
+            'vhost' => '/',                     //管理，这玩意儿相当于mysql的数据库
+            'port' => 5672,                      //端口
+            'login' => 'guest',                //账号
+            'password' => 'guest',               //密码
+            'heartbeat' => 30,                  //心跳
+        ],
+        "queue"=>[
+            "collectorder.route"=>[                     //路由 key
+                'exchange'=>'collectorder.exchange',  //交换区名称
+                'queue'=>'collectorder.queue',         //队列名
+                'exchangeType'=>'direct',  //交换机类型
+                'exchangeFlags'=>2,         //交换机标志
+                'queueFlags'=>2             //队列标志
+            ]
+        ]
     ]
 
 ];
