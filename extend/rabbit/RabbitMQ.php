01b52d1291d113426b0eb2c2bc59da47690aee12
StilesZ <1350054642@22.com>
2022-03-18 23:50:03 +0800

    ack 消息推送监听 confirm机制

--- extend/rabbit/RabbitMQ.php
+++ extend/rabbit/RabbitMQ.php
@@ -16,11 +16,11 @@ class RabbitMQ
 
     static private $instance;
 
-    private   $host     = '127.0.0.1';
-    private   $port     = 5672;
-    private   $user     = 'guest';
-    private   $password = 'guest';
-    private   $vhost    = '/';
+    private $host = '127.0.0.1';
+    private $port = 5672;
+    private $user = 'guest';
+    private $password = 'guest';
+    private $vhost = '/';
     protected $connection;
     protected $channel;
 
@@ -28,7 +28,7 @@ class RabbitMQ
      * RabbitMQ constructor.
      * @param array $config
      */
-    public function __construct(array $config = [])
+    private function __construct(array $config = [])
     {
         !empty($config['host']) && $this->host = $config['host'];
         !empty($config['port']) && $this->host = $config['port'];
@@ -36,8 +36,9 @@ class RabbitMQ
         !empty($config['password']) && $this->host = $config['password'];
         !empty($config['vhost']) && $this->host = $config['vhost'];
 
-        $this->connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password, $this->vhost);
-        $this->channel    = $this->connection->channel();
+        $this->connection = new AMQPStreamConnection($this->host, $this->port, $this->user, $this->password,
+            $this->vhost);
+        $this->channel = $this->connection->channel();
     }
 
     /**
@@ -70,12 +71,35 @@ class RabbitMQ
     }
 
     /**
+     * 监听 confirm
+     * @return AMQPChannel
+     */
+    public function setConfirm()
+    {
+        //推送成功
+        $this->channel->set_ack_handler(
+            function (AMQPMessage $message) {
+                echo 'Message acked ' . $message->body . PHP_EOL;
+            }
+        );
+
+        //推送失败
+        $this->channel->set_nack_handler(
+            function (AMQPMessage $message) {
+                echo 'Message nacked ' . $message->body . PHP_EOL;
+            }
+        );
+
+        $this->channel->confirm_select();
+    }
+
+    /**
      * 声明一个交换器
      * @param string $exchangeName 名称
-     * @param string $type         交换机类型 ：直连交换机（direct） 主题交换机 （topic） 头交换机（headers） 扇交换机（fanout）
-     * @param bool   $pasive       是否检测同名队列
-     * @param bool   $durable      是否开启队列持久化
-     * @param bool   $autoDelete   通道关闭后是否删除队列
+     * @param string $type 交换机类型 ：直连交换机（direct） 主题交换机 （topic） 头交换机（headers） 扇交换机（fanout）
+     * @param bool $pasive 是否检测同名队列
+     * @param bool $durable 是否开启队列持久化
+     * @param bool $autoDelete 通道关闭后是否删除队列
      */
     public function createExchange(
         string $exchangeName,
@@ -83,20 +107,19 @@ class RabbitMQ
         bool $pasive = false,
         bool $durable = false,
         bool $autoDelete = false
-    )
-    {
+    ) {
         $this->channel->exchange_declare($exchangeName, $type, $pasive, $durable, $autoDelete);
     }
 
     /**
      * 创建队列
      * @param string $queueName
-     * @param bool   $pasive
-     * @param bool   $durable
-     * @param bool   $exlusive
-     * @param bool   $autoDelete
-     * @param bool   $noWait
-     * @param array  $arguments
+     * @param bool $pasive
+     * @param bool $durable
+     * @param bool $exlusive
+     * @param bool $autoDelete
+     * @param bool $noWait
+     * @param array $arguments
      * @return mixed|null
      */
     public function createQueue(
@@ -107,8 +130,7 @@ class RabbitMQ
         bool $autoDelete = false,
         bool $noWait = false,
         array $arguments = []
-    )
-    {
+    ) {
         $args = [];
         if (!empty($arguments)) {
             $args = new AMQPTable();
@@ -122,11 +144,11 @@ class RabbitMQ
 
     /**
      * 绑定队列
-     * @param string   $queue
-     * @param string   $exchangeName
-     * @param string   $routeKey
-     * @param bool     $noWait
-     * @param array    $arguments
+     * @param string $queue
+     * @param string $exchangeName
+     * @param string $routeKey
+     * @param bool $noWait
+     * @param array $arguments
      * @param int|null $ticket
      */
     public function bindQueue(
@@ -136,21 +158,25 @@ class RabbitMQ
         bool $noWait = false,
         array $arguments = [],
         $ticket = null
-    )
-    {
+    ) {
         $this->channel->queue_bind($queue, $exchangeName, $routeKey, $noWait, $arguments, $ticket);
     }
 
     /**
      * 生成信息
-     * @param string $message    消息体
-     * @param string $exchange   交换机
-     * @param string $routeKey   路由key
-     * @param array  $properties 属性
-     * @param array  $headers
+     * @param string $message 消息体
+     * @param string $exchange 交换机
+     * @param string $routeKey 路由key
+     * @param array $properties 属性
+     * @param array $headers
      */
-    public function producerMessage(string $message, string $exchange, string $routeKey, array $properties = [], array $headers = [])
-    {
+    public function producerMessage(
+        string $message,
+        string $exchange,
+        string $routeKey,
+        array $properties = [],
+        array $headers = []
+    ) {
         $data = new AMQPMessage($message, $properties);
 
         if (!empty($headers)) {
@@ -166,10 +192,10 @@ class RabbitMQ
      * @param string $queueName
      * @param        $callback
      * @param string $tag
-     * @param bool   $noLocal
-     * @param bool   $noAck
-     * @param bool   $exclusive
-     * @param bool   $noWait
+     * @param bool $noLocal
+     * @param bool $noAck
+     * @param bool $exclusive
+     * @param bool $noWait
      * @throws ErrorException
      */
     public function consumeMessage(
@@ -180,13 +206,12 @@ class RabbitMQ
         bool $noAck = false,
         bool $exclusive = false,
         bool $noWait = false
-    )
-    {
+    ) {
         //只有consumer已经处理并确认了上一条message时queue才分派新的message给它
         $this->channel->basic_qos(null, 1, null);
         $this->channel->basic_consume($queueName, $tag, $noLocal, $noAck, $exclusive, $noWait, $callback);
 //        while ($this->channel->is_consuming()) {
-            $this->channel->wait();
+        $this->channel->wait();
 //        }
     }
 
